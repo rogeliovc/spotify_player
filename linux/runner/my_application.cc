df0f24d09a5b554b449ac4dd1b91e749e14f1f3e
gaboLectric <galcarazsu@gmail.com>
2025-05-14 21:39:23 -0600

    chore: update app name

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "spotify_player");
+    gtk_header_bar_set_title(header_bar, "Sincronía");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "spotify_player");
+    gtk_window_set_title(window, "Sincronía");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
